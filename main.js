a4c016e78bd0a1e15c473aa4f7ea1eb69862c8ba
ADAM AMADI <ADAM.AMADI22@GMAIL.COM>
2020-07-02 16:41:54 -0500

    finished

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Starter code for Create Your Own Array Methods Activity:
 
 // sample arrays for testing your functions:
+// sample arrays for testing your functions:
 let testNumsArray = [3, 1, -5, 2, 105, -24, 495, 8, 246]
 let testWordsArray = ['potato', 'rainbow', 'cloud', 'duck', 'orange', 'three']
-
 // This is one example of implementing the functionality of forEach:
 function newForEach(array, callback){
     for (let index = 0; index < array.length; index = index + 1){
@@ -11,28 +11,59 @@ function newForEach(array, callback){
         callback(currentItem, index, array)
     }
 }
-
 // complete the functions below so that they mimic the built in methods
-function newSome() {
+function newSome(array, callback) {
+    for(let index = 0; index < array.length; index = index + 1){
+        let currentItem = array[index]
+        if(callback(currentItem) == true){
+            return true
+        }
+    }
+    return false
+}
+function newEvery(array, callback) {
+    for(let index = 0; index < array.length; index = index + 1){
+        let currentItem = array[index]
+        if(callback(currentItem) == false){
+            return false
+        }
+    }
+    return true
 
 }
-
-function newEvery() {
-
+function newFind(array, callback) {
+    for(let index = 0; index < array.length; index = index + 1){
+        let currentItem = array[index]
+        if(callback(currentItem) == true){
+            return currentItem
+        }
+    }
+    return undefined
 }
-
-function newFind() {
-
+function newFindIndex(array, callback) {
+    for(let index = 0; index < array.length; index = index + 1){
+        let currentItem = array[index]
+        if(callback(currentItem) == true){
+            return index
+        }
+    }
+    return -1
 }
-
-function newFindIndex() {
-
+function newMap(array, callback) {
+    let resultArray = []
+    for(let index = 0; index < array.length; index = index + 1){
+        let currentItem = array[index]
+        resultArray.push(callback(currentItem))
+    }
+    return resultArray
 }
-
-function newMap() {
-
-}
-
-function newFilter() {
-
+function newFilter(array, callback) {
+    let resultArray = []
+    for(let index = 0; index < array.length; index = index + 1){
+        let currentItem = array[index]
+        if(callback(currentItem)){
+            resultArray.push(currentItem)
+        }
+    }
+    return resultArray
 }
